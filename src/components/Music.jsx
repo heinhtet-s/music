9f9da80efd171ddd367301c2c0680e5bec553031
heinhtet-s <heinh9540@gmail.com>
2023-01-10 18:45:17 +0630

    err-fix

--- src/components/Music.jsx
+++ src/components/Music.jsx
@@ -6,17 +6,45 @@ import img5 from '../assets/5.jpg'
 import img6 from '../assets/6.jpg' 
 import img7 from '../assets/7.jpg' 
 import img8 from '../assets/3.jpg' 
+import music1 from '../assets/1.wav'
+import music2 from '../assets/22.wav'
+import music3 from '../assets/3.wav'
+import music4 from '../assets/4.wav'
+import music5 from '../assets/5.wav'
+import music6 from '../assets/6.wav'
 import React from 'react'
 import  classes from './Music.module.css'
 import MusicSlider from './MusicSlider'
 import Data from './MusicData'
 import { useNavigate } from 'react-router-dom'
+import ReactAudioPlayer from 'react-audio-player'
 
 
 function Music() {
   const navigate=useNavigate();
-  const detailHandler = (data) => {
-
+  const detailHandler = (id) => {
+    switch (id) {
+      case "music1":
+          return music1;
+          break;
+      case "music2":
+          return music2;
+          break;
+      case "music3":
+          return music3;
+          break;
+      case "music4":
+          return music4;
+          break;
+      case "music5":
+          return music5;
+          break;
+      case "music6":
+          return music6;
+          break;
+      default:
+          return music1;
+  }
   }
   return (
     <div className={classes.container}>
@@ -32,10 +60,25 @@ function Music() {
             <div>
             <img src={img1} />
             </div>
+            <div style={{width: "100%",paddingLeft: "10px",paddingRight: "10px" }} >
             <p className={classes.song}>{item.title}</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita facilis molestias, quas neque porro necessitatibus ipsam eius illo distinctio esse, beatae recusandae. Cumque natus optio at nesciunt id. Esse, sint!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, 
+              </p>
+              <ReactAudioPlayer
+      src={()=>detailHandler( item.song )}
+  style={{width: '100%'}}
+  controls
+/>
             <div className={classes.details}>
-              <p onClick={()=>navigate(`/detail/${item.song}`)}>Details</p>
-              <p>Buy</p>
+              <p>
+              </p>
+              <button className={classes.music_btn} style={{    marginLeft: "auto", 
+    display: "block",}}>
+    Buy
+</button>
+            </div>
+            
             </div>
           </div>
            )
